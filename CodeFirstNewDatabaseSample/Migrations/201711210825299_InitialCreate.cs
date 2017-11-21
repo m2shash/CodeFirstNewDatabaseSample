c2e239e0d4025eb4df8ef9c1f58a10db9d70dd06
m2shash <m2shash@gmail.com>
2017-11-21 14:46:17 +0200

    checked in the project

---
+++ CodeFirstNewDatabaseSample/Migrations/201711210825299_InitialCreate.cs
@@ -0,0 +1,52 @@
+namespace CodeFirstNewDatabaseSample.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        BlogId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.BlogId);
+            
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        PostId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Content = c.String(),
+                        BlogId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PostId)
+                .ForeignKey("dbo.Blogs", t => t.BlogId, cascadeDelete: true)
+                .Index(t => t.BlogId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Username = c.String(nullable: false, maxLength: 128),
+                        DisplayName = c.String(),
+                    })
+                .PrimaryKey(t => t.Username);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Posts", "BlogId", "dbo.Blogs");
+            DropIndex("dbo.Posts", new[] { "BlogId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Posts");
+            DropTable("dbo.Blogs");
+        }
+    }
+}
---
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Models;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Name = "Another Blog " });
+                db.SaveChanges();
+
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(blog.Name);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.DisplayName)
+                .HasColumnName("display_name");
+        }
+    }
+}
